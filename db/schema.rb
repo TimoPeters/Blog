d0c751cd0f5e66efa3d4cc7fe83ab7061437bea8
timpeter <timo.peters@digitaspixelpark.com>
2020-02-27 15:43:09 +0100

    added author column to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_162920) do
+ActiveRecord::Schema.define(version: 2020_02_27_094902) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
   create_table "comments", force: :cascade do |t|
